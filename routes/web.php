f652885da9fa65fcff08dd217ef22b9dab402cb4
oscarLS-11 <oluna1103@gmail.com>
2022-04-27 10:13:24 -0500

    equisde

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// perdon profe no recorde que lo podía ver xd
+// perdon profe no recorde que lo podía ver xdd
 
 Route::get('/', function () {
     return view('welcome');
