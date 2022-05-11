470a751b8d3f84ef6812c246b2db4dc3e1ccb136
jorgeluis1122 <jorgeluissebastianh@gmail.com>
2022-05-11 09:53:45 -0500

    aja

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
-//<<<<<<< HEAD
 use App\Http\Controllers\ArticuloController;
-<<<<<<< HEAD
-//12
-=======
-//marck
->>>>>>> 1641c2a0e2a307cc2fa02d5d9430f4acaf21077f
-//1
 Route ::resources([
-'articulos' => ArticuloController::class,
-]);
-<<<<<<< HEAD
-// emanem=======
-=======
-// emanems
-//======
->>>>>>> 1641c2a0e2a307cc2fa02d5d9430f4acaf21077f
+'articulos' => ArticuloController::class,]);
 use App\Http\Controllers\MateriaPrimaController;
-
 Route::resources([
     'materiaprimas' => MateriaPrimaController::class
 ]);
-//>>>>>>> be7743ee62fdef2eedc02818df4351c62a69aa4d
-
 Route::get('/', function () {
     return view('welcome');
 });
